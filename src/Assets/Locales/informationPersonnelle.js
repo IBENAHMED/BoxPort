f68c6031e3ee49ee9495bc5885cb42d3fbb06815
Reda IBENAHMED <ibenahmed.reda05@gmail.com>
2023-07-18 00:07:30 +0100

    bodys

--- src/Assets/Locales/informationPersonnelle.js
+++ src/Assets/Locales/informationPersonnelle.js
@@ -4,7 +4,7 @@ export const DataItem = [
     {
         "icon": <MdDeveloperBoard />,
         "titre": "Web Developer",
-        "text": "Développer des sites Web ou d'autres applications multimédias. Gestion des bases de données et des frameworks les plus célèbres comme React et Spring Boot"
+        "text": " Développer des sites Web ou d'autres applications multimédias. Gestion des bases de données et des frameworks les plus célèbres comme React et Spring Boot"
     },
     {
         "icon": <MdOutlineCastForEducation />,
